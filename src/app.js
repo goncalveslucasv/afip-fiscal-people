0a9c48015dce70541ce8eb2726ca9e53243b2a34
Lucas Goncalves <lucasgoncalves@MacBook-Air-de-Lucas.local>
2019-04-19 19:52:51 -0300

    0.0.2

--- src/app.js
+++ src/app.js
@@ -7,21 +7,21 @@ const unzip = require('unzipper');
 const { Transform, PassThrough } = require('stream');
 
 function mapToObject(chunk) {
-	const _substr = data => ({ from, length }) => data.substr(from, length);
-	const dataSubstr = _substr(chunk.toString());
+	const explicitSubstr = data => ({ from, length }) => data.substr(from, length);
+	const dataSubstr = explicitSubstr(chunk.toString());
 	return {
-		_id: dataSubstr({ from: 0, length: 11 }),
+		id: dataSubstr({ from: 0, length: 11 }),
 		denomination: dataSubstr({ from: 11, length: 30 }),
-		income_tax: dataSubstr({ from: 41, length: 2 }),
+		incomeTax: dataSubstr({ from: 41, length: 2 }),
 		iva: dataSubstr({ from: 43, length: 2 }),
-		monotributo: dataSubstr({ from: 45, length: 2 }),
-		soc: dataSubstr({ from: 47, length: 1 }),
+		monotax: dataSubstr({ from: 45, length: 2 }),
+		socMember: dataSubstr({ from: 47, length: 1 }),
 		employer: dataSubstr({ from: 48, length: 1 }),
-		activity: dataSubstr({ from: 50, length: 2 })
+		monotaxActivity: dataSubstr({ from: 50, length: 2 })
 	};
 }
 
-function getFiscalPeople(file, options = {}) {
+module.exports = function getFiscalPeople(file, options = {}) {
 	const transformData = new Transform({
 		writableObjectMode: true,
 		objectMode: true,
@@ -59,9 +59,9 @@ function getFiscalPeople(file, options = {}) {
 			options.target &&
       typeof options.target === 'string' &&
       options.target.startsWith('mongodb')
-		)
-
-			console.log(typeof options.target);
+		) {
+			// todo mongodb streaming
+		}
 
 		return processStream;
 	};
@@ -77,11 +77,4 @@ function getFiscalPeople(file, options = {}) {
 
 	return stream;
 
-}
-
-// usage
-getFiscalPeople('http://www.afip.gob.ar/genericos/cInscripcion/archivos/apellidoNombreDenominacion.zip',
-	{
-		target: () => fs.createWriteStream(`out2.txt`)
-	}
-).on('error', err => { console.log('my error', err); });
+};
